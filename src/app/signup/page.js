7a0a0e5c1d674f52038027d7366505a4f9149512
Advait Variyar <variyar@usc.edu>
2025-06-09 23:34:20 -0700

    Add Routing and CSS

---
+++ src/app/signup/page.js
@@ -0,0 +1,9 @@
+import styles from "../styles/signup.module.css";
+
+export default function Signup() {
+  return (
+    <div className={styles.page}>
+      <p>Signup</p>
+    </div>
+  );
+}
---
+++ src/app/user/page.js
@@ -0,0 +1,9 @@
+import styles from "../styles/user.module.css";
+
+export default function User() {
+  return (
+    <div className={styles.page}>
+      <p>User</p>
+    </div>
+  );
+}
